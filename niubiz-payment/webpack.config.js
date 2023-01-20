dfeaf6e74491ccb93e64fe24ae99a252d83506a2
Max Aguilar <zz30.maxzz@gmail.com>
2023-01-20 02:20:01 -0500

    Uploading react project to build niubiz payment

---
+++ niubiz-payment/webpack.config.js
@@ -0,0 +1,23 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.tsx",
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        use: "ts-loader",
+      },
+    ],
+  },
+  output: {
+    path: path.resolve(__dirname, "./dist"),
+    filename: "bundle.js",
+  },
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, "./dist"),
+    },
+  },
+};
